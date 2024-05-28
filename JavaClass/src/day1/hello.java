560d5333700d56595f40a5e46ebdf32c324a715e
KGM <KGM@CASL6-39-167.kgisledu.com>
2024-05-28 03:29:26 -0700

    day-2 -> (if else)

--- JavaClass/src/day1/hello.java
+++ JavaClass/src/day1/hello.java
@@ -3,7 +3,8 @@ package day1;
 public class hello {
 
 	public static void main(String[] args) {
-		System.out.println("hello world");
+		int a = 6;
+		System.out.println("hello world "+ a);
 
 	}
 
---
+++ JavaClass/src/day2/voting.java
@@ -0,0 +1,21 @@
+package day2;
+
+import java.util.Scanner;
+
+public class voting {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter the age");
+		int age = sc.nextInt();
+		
+		if(age>18) {
+			System.out.println("you can vote");
+		}
+		else {
+			System.out.println("you cannot vote");
+		}
+
+	}
+
+}
